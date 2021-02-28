b5b5170c027b3a6be3808cf5a6b600256fd2842c
K-Philleani <964202623@qq.com>
2021-02-28 23:45:22 +0800

    杨辉三角

---
+++ 算法/杨辉三角.js
@@ -0,0 +1,19 @@
+
+
+var generate = function(numRows) {
+	if (numRows == 0) return []
+	const arr = []
+	for (let i = 0; i < numRows; i++) {
+		arr[i] = []
+		for (let j = 0; j <= i; j++) {
+			if (j ==0 || i ==j) {
+				arr[i][j] = 1
+			} else {
+				arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+			}
+		}
+	}
+	return arr
+};
+
+console.log(generate(5))
