a7352bbe17fc28fa0e41f203c3adfd13d7c8b2b1
K-Philleani <964202623@qq.com>
2021-03-15 23:29:47 +0800

    数组分块

---
+++ 数组分块/index.js
@@ -0,0 +1,22 @@
+function chunk(arr, size = 1) {
+	if (arr.length === 0) {
+		return []
+	}
+	// 声明两个变量
+	let result = []
+	let tmp = []
+	// 遍历
+	arr.forEach(item => {
+		// 判断tmp元素的长度是否为0
+		if (tmp.length === 0) {
+			// 将tmp压入到result中
+			result.push(tmp)
+		}
+		// 将元素压入到历史数组tmp中
+		tmp.push(item)
+		if (tmp.length === size) {
+			tmp = []
+		}
+	})
+	return result
+}
