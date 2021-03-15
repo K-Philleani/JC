8feee2cae9a5d3146b410b8613cf221706b73ab1
K-Philleani <964202623@qq.com>
2021-03-16 00:01:34 +0800

    解析Url参数

---
+++ 解析Url参数/index.js
@@ -0,0 +1,16 @@
+function UrlSearchParams(url) {
+	if (url.indexOf('?') !== -1) {
+		let urlSearch = url.split('?')[1]
+		let tmpObj = {}
+		let strs = urlSearch.split('&');
+		for (let i =0; i < strs.length; i++) {
+			tmpObj[strs[i].split('=')[0]] = strs[i].split('=')[1]
+		}
+		return tmpObj
+	}
+}
+
+
+
+let url = "https://www.javascript.com/?foo=ab&bar=cd&cat=ddf"
+console.log(UrlSearchParams(url))
